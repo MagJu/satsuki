e8b775a868ca15a0f1ca4659ab5bce3c8fb9634b
MagJu <magjuyt@gmail.com>
2019-12-17 01:50:17 +0100

    Optimisation

---
+++ main.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const { TOKEN, PREFIX } = require("./config");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log("Le bot est connecté");
+});
+
+client.on('message', msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping!");
+});
+
+client.login(TOKEN);
